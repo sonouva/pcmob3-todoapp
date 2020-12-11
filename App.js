8e130b4ebbac44bffb3395a140afaa4b75c84871
junjie <jun_jie1994@hotmail.com>
2020-12-11 12:57:50 +0800

    to do list project

--- App.js
+++ App.js
@@ -1,14 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {createStackNavigator} from "@react-navigation/stack";
-import {NavigationContainer} from "@react-navigation/native";
-import {TouchableOpacity} from "react-native-gesture-handler";
-import {Ionicons} from "@expo/vector-icons";
-import * as SQLite from 'expo-sqlite';
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+import * as FileSystem from "expo-file-system";
 
 
-function NotesScreen({ navigation }) {
+
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator headerMode="none" mode="modal">
+        <Stack.Screen name="Notes Stack" component={NotesStack} />
+        <Stack.Screen name="Add Screen" component={AddScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function AddScreen({ navigation }) {
+  const [text, setText] = useState("");
+
+  return (
+    <View style={[styles.container, { backgroundColor: "white" }]}>
+      <Text style={{ fontSize: 24 }}>What do you want to add?</Text>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={(input) => setText(input)}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Notes", { text })}
+        >
+          <Text style={styles.buttonText}>Save</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={styles.buttonText}>Dismiss</Text>
+        </TouchableOpacity>
+      </View>
+      {/* <Text>{text.toUpperCase()}</Text> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderColor: "grey",
+    borderWidth: 1,
+    width: "80%",
+    padding: 10,
+    marginTop: 20,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "orange",
+    borderRadius: 5,
+    margin: 10,
+    marginTop: 30,
+    width: 80,
+  },
+  buttonText: {
+    textAlign: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+  },
+});
+
+const db = SQLite.openDatabase("notes.db");
+console.log(FileSystem.documentDirectory);
+
+export default function NotesScreen({ navigation, route }) {
+  const [notes, setNotes] = useState([]);
+
+  function refreshNotes() {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM notes",
+        null,
+        (txObj, { rows: { _array } }) => setNotes(_array),
+        (txObj, error) => console.log(`Error: ${error}`)
+      );
+    });
+  }
+
+  // This is to set up the database on first run
+  useEffect(() => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          `CREATE TABLE IF NOT EXISTS notes
+        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT,
+          done INT)
+        `
+        );
+      },
+      null,
+      refreshNotes
+    );
+  }, []);
+
+  // This is to set up the top right button
   useEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -27,47 +139,50 @@ function NotesScreen({ navigation }) {
     });
   });
 
+  // Monitor route.params for changes and add items to the database
+  useEffect(() => {
+    if (route.params?.text) {
+      db.transaction(
+        (tx) => {
+          tx.executeSql("INSERT INTO notes (done, title) VALUES (0, ?)", [
+            route.params.text,
+          ]);
+        },
+        null,
+        refreshNotes
+      );
+    }
+  }, [route.params?.text]);
+
   function addNote() {
-    console.log("Adding a new note!");
+    navigation.navigate("Add Screen");
+  }
+
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text>{item.title}</Text>
+      </View>
+    );
   }
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={notes}
+        renderItem={renderItem}
+        style={{ width: "100%" }}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
-  );
-}
-
-const db = SQLite.openDatabase("notes.db");
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Notes"
-          component={NotesScreen}
-          options={{
-            title: "To do App",
-            headerStyle: {
-              backgroundColor: "yellow",
-              height: 100,
-              shadowColor: "black",
-              shadowOpacity: 0.2,
-              shadowRadius: 5,
-            },
-            headerTintColor: "#5437",
-            headerTitleStyle: {
-              fontSize: 24,
-              fontWeight: "bold",
-            },
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
   );
 }
 
@@ -79,3 +194,30 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const InnerStack = createStackNavigator();
+function NotesStack() {
+  return (
+    <InnerStack.Navigator>
+      <InnerStack.Screen
+        name="Notes"
+        component={NotesScreen}
+        options={{
+          title: "To do App",
+          headerStyle: {
+            backgroundColor: "yellow",
+            height: 100,
+            shadowColor: "black",
+            shadowOpacity: 0.2,
+            shadowRadius: 5,
+          },
+          headerTintColor: "#f55",
+          headerTitleStyle: {
+            fontSize: 24,
+            fontWeight: "bold",
+          },
+        }}
+      />
+    </InnerStack.Navigator>
+  );
+}
